c84471cab46e4bdf575256af5f61ad9924a0f790
Rafaela Polizel <rafaela.polizel@pca.com.br>
2020-08-17 23:56:01 -0300

    :construction: Added some examples of routes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,49 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({message: 'Hello World!'});
+app.use(express.json());
+
+app.get('/projects', (req, res) => {
+  console.log(req.query);
+
+  return res.json([
+    'Project 1',
+    'Project 2',
+    'Project 3'
+  ]);
+});
+
+app.post('/projects', (req, res) => {
+  const {title, owned} = req.body;
+  console.log(title, owned);  
+
+  return res.json([
+    'Project 1',
+    'Project 2',
+    'Project 3',
+    'Project 4'
+  ]);
+});
+
+app.put('/projects/:id', (req, res) => {
+  console.log(req.params);
+
+  return res.json([
+    'Project 5',
+    'Project 2',
+    'Project 3',
+    'Project 4'
+  ]);
+});
+
+app.delete('/projects/:id', (req, res) => {
+  console.log(req.params);
+
+  return res.json([
+    'Project 2',
+    'Project 3',
+    'Project 4'
+  ]);
 });
 
 app.listen(3333, () => console.log('Back-end started!'));
