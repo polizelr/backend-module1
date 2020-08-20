0da5796696d829f49e65844bdb9ad8f80b124e21
Rafaela Polizel <rafaela.polizel@pca.com.br>
2020-08-19 21:35:19 -0300

    :construction: Added middlewares

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-const { uuid } = require('uuidv4');
-
+const { uuid, isUuid } = require('uuidv4');
 const express = require('express');
 
 const app = express();
@@ -7,6 +6,30 @@ const app = express();
 app.use(express.json());
 
 const projects = [];
+
+function logRequests (req, res, next) {
+  const { method, url} = req;
+
+  const label = `[${method.toUpperCase()}] ${url}`
+
+  console.log(label);
+
+  return next();
+}
+
+function validateProjectId (req, res, next) {
+  const {id} = req.params;
+
+  if(!isUuid(id)) {
+    return res.status(400).json({error: "Invalid project ID."});
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+
+app.use('/projects/:id', validateProjectId);
 
 app.get('/projects', (req, res) => {
 
